6df8ac33b2b94461f714134de6d23dc1644a90ab
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2022-03-17 08:54:31 -0300

    mesa aula 19

---
+++ mesa_aula_19/src/com/ctd/Main.java
@@ -0,0 +1,33 @@
+package com.ctd;
+
+import com.ctd.model.Contato;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Contato c1 = new Contato("jorge", "jorge@gmail.com", "5521454661");
+        Contato c2 = new Contato("Maria", "Maria@gmail.com", "84166615444");
+        Contato c3 = new Contato("Lia", "Lia@gmail.com", "84914134777");
+
+        List<Contato> contatos = Arrays.asList(c1, c2, c3);
+
+        /*Salvar o objeto em um arquivo*/
+        FileOutputStream fo = new FileOutputStream("listaDePaginas.txt");
+        ObjectOutputStream oos = new ObjectOutputStream(fo);
+        oos.writeObject(contatos);
+
+
+        /*Recuperar o objeto de um arquivo*/
+        FileInputStream fi = new FileInputStream("listaDePaginas.txt");
+        ObjectInputStream ois = new ObjectInputStream(fi);
+
+        System.out.println(ois.readObject());
+
+        ois.close();
+
+    }
+}
