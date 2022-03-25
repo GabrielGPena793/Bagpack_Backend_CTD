3e6b3c0e94d9315d58a389434888669b899dc60e
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2022-03-25 13:41:39 -0300

    atualizando crud da aula 21 Paciente

--- aula21/aula21/src/main/java/com/springDh/aula21/services/PacienteService.java
+++ aula21/aula21/src/main/java/com/springDh/aula21/services/PacienteService.java
@@ -3,6 +3,8 @@ package com.springDh.aula21.services;
 import com.springDh.aula21.dao.IDao;
 import com.springDh.aula21.model.Paciente;
 
+import java.util.List;
+
 public class PacienteService {
 
     private IDao<Paciente> pacienteIDao;
@@ -13,5 +15,21 @@ public class PacienteService {
 
     public Paciente salvar(Paciente paciente){
         return pacienteIDao.salvar(paciente);
+    }
+
+    public Paciente buscarPorId(Integer id){
+        return pacienteIDao.buscar(id).orElse(null);
+    }
+
+    public void excluir(Integer id){
+        pacienteIDao.excluir(id);
+    }
+
+    public List<Paciente> buscarTodos(){
+        return pacienteIDao.buscarTodos();
+    }
+
+    public Paciente atualizar (Paciente paciente){
+        return pacienteIDao.atualizar(paciente);
     }
 }
